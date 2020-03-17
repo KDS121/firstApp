4149a11229e3b770e7f93dc131eb783971d176be
Karan <guitaristkaran@gmail.com>
2020-03-17 11:12:52 +0530

    first commit

---
+++ src/components/Cards.vue
@@ -0,0 +1,136 @@
+<template>
+<div class="cards" > 
+    <v-col cols="6" sm="4" v-for="item in items" :key="item.pname">
+    <v-hover
+        v-slot:default="{ hover }"
+        close-delay="200"
+      >
+      <v-card
+       :elevation="hover ? 16 : 2"
+       :loading="loading"
+       class="mx-auto my-12"
+       max-width="374"
+       style="border-radius:16px"
+       >
+    <v-img
+      height="250"
+      src="{{item.img}}"
+    ></v-img>
+
+    <v-card-title>{{item.pname}}</v-card-title>
+
+    <v-card-text>
+      <v-row
+        align="center"
+        class="mx-0"
+      >
+        <v-rating
+          :value=4
+          color="amber"
+          dense
+          half-increments
+          readonly
+          size="14"
+        ></v-rating>
+
+        <div class="grey--text ml-4">4.5 ({{item.numberOfPeopleRated}})</div>
+      </v-row>
+
+      
+
+      <div>{{item.description}}</div>
+    </v-card-text>
+
+    <v-divider class="mx-4"></v-divider>
+
+  
+
+    <v-card-text>
+      <v-chip-group
+        
+        column
+        
+
+      >
+        <v-chip 
+      color="#3389C2"
+      text-color="white">react</v-chip>
+
+        <v-chip color="#3389C2" text-color="white">{{item.tags}}</v-chip>
+
+      </v-chip-group>
+    </v-card-text>
+
+    <v-card-actions>
+      <v-btn 
+        rounded
+        color="#2496FF" dark
+        text
+        @click="reserve"
+      >
+        Read More
+      </v-btn>
+    </v-card-actions>
+  </v-card>
+       </v-hover>
+    </v-col>
+</div>
+</template>
+<script>
+export default {
+    data(){
+        return {
+            items : [
+                { 
+                 img: 'https://cdn.vuetifyjs.com/images/cards/cooking.png' , 
+                 pname: 'Project Title',
+                 rating: 4, 
+                 numberOfPeopleRated: 143,
+                 description : 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.', 
+                 tags: ["javascript", "css","html"]
+                }
+               ,{
+                 img: 'https://cdn.vuetifyjs.com/images/cards/cooking.png' , 
+                 pname: 'Project Title',
+                 rating: 4, 
+                 numberOfPeopleRated: 143,
+                 description : 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.', 
+                 tags: ["javascript", "css","html"]
+                 }
+               ,{
+                  img: 'https://cdn.vuetifyjs.com/images/cards/cooking.png' , 
+                 pname: 'Project Title',
+                 rating: 4, 
+                 numberOfPeopleRated: 143,
+                 description : 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.', 
+                 tags: ["javascript", "css","html"] 
+                 }
+                ,{
+                    img: 'https://cdn.vuetifyjs.com/images/cards/cooking.png' , 
+                 pname: 'Project Title',
+                 rating: 4, 
+                 numberOfPeopleRated: 143,
+                 description : 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.', 
+                 tags: ["javascript", "css","html"]
+                }
+                ,{
+                    img: 'https://cdn.vuetifyjs.com/images/cards/cooking.png' , 
+                 pname: 'Project Title',
+                 rating: 4, 
+                 numberOfPeopleRated: 143,
+                 description : 'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.', 
+                 tags: ["javascript", "css","html"]
+                }
+                ,{
+                 img: 'https://cdn.vuetifyjs.com/images/cards/cooking.png' , 
+                 pname: 'Project Title',
+                 rating: 4, 
+                 numberOfPeopleRated: 143,
+                 description :'Small plates, salads & sandwiches - an intimate setting with 12 indoor seats plus patio seating.', 
+                 tags: ["javascript", "css","html"]
+                }
+            ]
+        }
+    }
+}
+</script>
---
+++ src/components/Home.vue
@@ -0,0 +1,86 @@
+<template>
+<nav>
+    <v-app-bar
+      color="FFFFFF"
+      fixed
+    >
+      <v-app-bar-nav-icon @click="drawer =! drawer"> </v-app-bar-nav-icon>
+      <v-img src="../assets/icon.svg"
+       max-height="60"
+       contain
+       class=icon>
+       </v-img>
+      <v-text-field 
+        flat
+        solo-inverted
+        hide-details
+        prepend-inner-icon="mdi-magnify"
+        label="Search.."
+        class="hidden-sm-and-down searchbox"
+      />
+      
+    <v-spacer></v-spacer>     
+    </v-app-bar>
+    
+    <v-navigation-drawer  v-model="drawer" app class="indogo xyz" color="#2496FF" width="80px"> 
+     <v-app-bar-nav-icon @click="drawer =! drawer" color="white" class="navicon mx-4 my-4"> </v-app-bar-nav-icon>
+    <v-list class="list" >
+        <v-list-title>
+            <v-list-title-action>
+                <v-img src="../assets/homeicon.svg"></v-img>            
+            </v-list-title-action>     
+            <v-list-title-action>
+                <v-img src="../assets/settings.svg"></v-img>            
+            </v-list-title-action>   
+            <v-list-title-action>
+                <v-img src="../assets/user.svg"></v-img>            
+            </v-list-title-action> 
+             <v-list-title-action>
+                <v-img src="../assets/phone.svg"></v-img>            
+            </v-list-title-action>   
+        </v-list-title>    
+    </v-list>
+    </v-navigation-drawer>
+  </nav>
+</template>
+
+<script>
+export default{
+    data(){
+        return{
+            drawer: false
+        }
+    }
+}
+</script>
+
+<style>
+.navicon{
+    transform : rotate(90deg);
+}
+.xyz{
+    box-shadow: 2px 0px 4px rgba(78, 78, 78, 0.25);
+    border-radius : 0px;
+    border:0;    
+}
+.searchbox{
+   position: absolute;
+left: 76.04%;
+right: 1.46%;
+top: 14.00%;
+bottom: 24.73%;
+background: #FAFAFA;
+border-radius: 11px;
+}
+.icon{
+ position: absolute;
+ width: 103.45px;
+ height: 50px;
+ left: 190px;
+ top: 4px;
+}
+.list{
+    width: 70%;
+    margin: 6px;
+}
+</style>
